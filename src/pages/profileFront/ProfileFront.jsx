e232797ca9dbd12ba30ca5c0adfcef1155865bfa
rifatptk <rifatptk@gmail.com>
2022-11-15 10:29:15 +0600

    share my details page done

---
+++ src/pages/profileFront/ProfileFront.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../../components/common/Container';
+
+const ProfileFront = () => {
+  return (
+    <div id="profileFront">
+      <Container>
+        <div className="relative font-inter w-full min-h-screen md:min-h-0 md:h-[792px] bg-white p-8"></div>
+      </Container>
+    </div>
+  );
+};
+
+export default ProfileFront;
